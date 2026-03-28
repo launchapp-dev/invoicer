303ff1f4474696d61e9c43a05e047fa7df46691c
Sami Shukri <samiziiv@gmail.com>
2026-03-28 04:52:58 -0600

    chore(db): remove dead src/lib/db.ts orphan SQLite connection [TASK-121] (#75)

--- src/lib/db.ts
+++
@@ -1,6 +0,0 @@
-import Database from "better-sqlite3";
-import { drizzle } from "drizzle-orm/better-sqlite3";
-import * as schema from "@/db/schema";
-
-const sqlite = new Database("./invoicer.db");
-export const db = drizzle(sqlite, { schema });
